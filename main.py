14df12fc737c8fc943e3bca5c4bffe3a0f9fd09d
inGeoscience <cake@sunnyra.in>
2022-08-23 18:45:01 +0800

    Fix

--- main.py
+++ main.py
@@ -6,27 +6,27 @@ from matplotlib import pyplot
 data = pandas.read_csv('ex1data2.txt', header=None, names=['Size', 'Bedrooms', 'Price'])
 # Mean Normalization
 data = (data - data.mean()) / data.std()
-# 构造特征值的矩阵、实际值的矩阵和参数矩阵
+# 构造特征值矩阵、实际值矩和参数的二维数组
 data.insert(0, 'ones', 1)
 colNum = data.shape[1]
 X = data.iloc[:, 0:colNum-1] # 这一步得到的是DataFrame
-X = numpy.matrix(X.values) # 将DataFrame转为Matrix
+X = numpy.matrix(X.values) # 将DataFrame的Values转为Matrix，坐标轴标签会被舍弃
 Y = data.iloc[:, colNum-1:colNum]
 Y = numpy.matrix(Y.values)
-theta = numpy.matrix(numpy.zeros(3)) # 有三个参数θ待求，转换成矩阵才能参与运算
+theta = numpy.zeros((1, colNum-1)) # theta为(1, 3)的二维数组。共有3个参数，包括截距项b在内
 # 代价函数
 def compCost(X, Y, theta):
     inner = numpy.power((X * theta.T) - Y, 2)
     return 1/(2*len(X))*numpy.sum(inner)
 # Batch Gradient Descent Function
 def batchGradientDescent(X, Y, theta, alpha, iters):
-    # 创建临时参数矩阵
-    temp_theta = numpy.matrix(numpy.zeros(theta.shape))
-    # 创建代价列表(ndarray)以记录每一次代价
+    # 创建临时参数二维数组
+    temp_theta = numpy.zeros(theta.shape)
+    # 创建代价列表(一维数组)以记录每一次代价，这里当作list来用
     cost = numpy.zeros(iters)
     for i in range(iters):
         error = X*theta.T - Y
-        # 计算每一个θ，并把θ添加到临时矩阵里
+        # 计算每一个θ，并把θ添加到临时参数向量里。enumerate()返回一个tuple，包含两个元素——第i行和值。
         for nth_para, para in enumerate(theta.T):
             # 每次累加中乘的x^(i)是一个标量，所以用的numpy.multiply()，而不是*
             para = para - ((alpha/len(X))*numpy.sum(numpy.multiply(error, X[:, nth_para])))
